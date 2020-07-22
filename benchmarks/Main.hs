419dab4a3d140b3bbf94581fabc85ccfa4af63ff
Michalis Pardalos <m.pardalos@gmail.com>
2020-07-22 17:50:59 +0100

    Add ByteArray# to benchmark

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -3,6 +3,7 @@
 import Criterion.Main
 import Criterion.Types
 import GHC.DataSize
+import Data.Primitive.ByteArray
 
 listBenchmark :: Benchmark
 listBenchmark = bgroup "[Int]"
@@ -17,8 +18,23 @@ listBenchmark = bgroup "[Int]"
     mkBenchmark :: Int -> Benchmark
     mkBenchmark size = env (pure [0 .. size]) $ \lst -> bench (show size) $ nfAppIO recursiveSize lst
 
+byteArrayBenchmark :: Benchmark
+byteArrayBenchmark = bgroup "ByteArray#"
+    (map mkBenchmark
+     [ 1000
+     , 10000
+     , 100000
+     , 1000000
+     , 10000000
+     ])
+  where
+    mkBenchmark :: Int -> Benchmark
+    mkBenchmark size = env
+      (newByteArray size)
+      (\lst -> bench (show size) $ nfAppIO recursiveSize lst)
+
 main :: IO ()
 main =
   defaultMainWith
     (defaultConfig {reportFile = Just "ghc-datasize-benchmark.html"})
-    [ listBenchmark ]
+    [ listBenchmark, byteArrayBenchmark ]
