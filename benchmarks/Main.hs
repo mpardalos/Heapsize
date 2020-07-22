ea4606c3a5cac3036f01978d6635d1b5f9a65429
Michalis Pardalos <m.pardalos@gmail.com>
2020-07-22 19:03:16 +0100

    Add a benchmark for small circular data structure

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -1,9 +1,15 @@
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
 
 import Criterion.Main
 import Criterion.Types
 import GHC.DataSize
+import GHC.Generics (Generic)
 import Data.Primitive.ByteArray
+import Data.IORef
+import Control.DeepSeq
 
 listBenchmark :: Benchmark
 listBenchmark = bgroup "[Int]" $
@@ -26,8 +32,23 @@ byteArrayBenchmark = bgroup "ByteArray#" $
       (newByteArray size)
       (\lst -> bench (show size) $ nfAppIO recursiveSize lst)
 
+
+data C = C Int (IORef C)
+  deriving (Generic, NFData)
+
+circularBenchmark :: Benchmark
+circularBenchmark = env mkT (\c -> bench "Circular" (nfAppIO recursiveSize c))
+  where
+    mkT :: IO C
+    mkT = do
+      rec first <- newIORef (C 1 second)
+          second <- newIORef (C 2 third)
+          third <- newIORef (C 3 first)
+      readIORef first
+
+
 main :: IO ()
 main =
   defaultMainWith
     (defaultConfig {reportFile = Just "ghc-datasize-benchmark.html"})
-    [ listBenchmark, byteArrayBenchmark ]
+    [ listBenchmark, byteArrayBenchmark, circularBenchmark ]
