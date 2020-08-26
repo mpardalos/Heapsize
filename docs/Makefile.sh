4aacf97e66ef3bbe6981fc442f8ca0af8f2d93ea
Michalis Pardalos <m.pardalos@gmail.com>
2020-08-26 19:21:48 +0300

    Remove docs, travis.yml, TODO.
    Dropping links to upstream

--- docs/Makefile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-for i in **/*.txt; do
-  asciidoc -b xhtml11 -f asciidoc.conf -a disable-javascript -a data-uri -a stylesheet=$PWD/asciidoc-toc.css -a idprefix -o - $i | hxtoc -l 2 > ${i:r}.html
-done
